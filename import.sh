057711c3a062a6c907b5e0d5bcca771eb0376fba
Nathan Carter <ncarter@bentley.edu>
2019-05-21 09:52:26 +0100

    Adding import script

---
+++ import.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Importing ge-lib.js from ../group-explorer-3/build/"
+cp ../group-explorer-3/build/ge-lib.js .
+echo "Importing all group files from ../group-explorer-3/groups/"
+cp ../group-explorer-3/groups/*.group ./groups/
+echo "Done."
+
