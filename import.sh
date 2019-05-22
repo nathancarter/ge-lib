b96e9da9c351accf2b48e43c5c217905459c6cff
Nathan Carter <ncarter@bentley.edu>
2019-05-22 14:34:01 +0100

    Updating scripts to work with new location of ge-lib.js file

--- import.sh
+++ import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Importing ge-lib.js from ../group-explorer-3/build/"
-cp ../group-explorer-3/build/ge-lib.js .
+cp ../group-explorer-3/build/ge-lib.js ./src/
 echo "Importing all group files from ../group-explorer-3/groups/"
 cp ../group-explorer-3/groups/*.group ./groups/
 echo "Done."
