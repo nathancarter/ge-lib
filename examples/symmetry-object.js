a76a7ec6f4eaad1bc5e2890658812f6fc48ff47a
Nathan Carter <ncarter@bentley.edu>
2019-05-23 15:16:02 +0100

    Update example code to match old (foggy) example renderings

--- examples/symmetry-object.js
+++ examples/symmetry-object.js
@@ -11,6 +11,9 @@ const SO = GE.SymmetryObject.generate( group, group.symmetryObjects[0].name );
 // Create a renderer for the symmetry object (can produce SVG, PDF, and PNG)
 const renderer = new GE.SymmetryObjectRenderer( SO );
 
+// Because the object we're rendering is 3D, it looks best with some fog:
+renderer.set( 'fogLevel', 0.75 );
+
 // To render an SVG/PDF/PNG, uncomment one of the lines below:
 // (But only one, lest they clobber one another's data asynchronously.)
 renderer.renderSVGFile( 'symmetry-object.svg' );
