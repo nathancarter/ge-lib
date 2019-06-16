c77efc5131c6e335bd59b7e40ecfc9e566d32d29
Nathan Carter <ncarter@bentley.edu>
2019-06-16 12:13:17 +0100

    Importing axis illustrations for use in docs

--- import.sh
+++ import.sh
@@ -4,5 +4,7 @@ echo "Importing ge-lib.js from ../group-explorer-3/build/"
 cp ../group-explorer-3/build/ge-lib.js ./src/
 echo "Importing all group files from ../group-explorer-3/groups/"
 cp ../group-explorer-3/groups/*.group ./groups/
+echo "Importing all axis icons from ../group-explorer-3/images/"
+cp ../group-explorer-3/images/axis*.png ./docs/
 echo "Done."
 
